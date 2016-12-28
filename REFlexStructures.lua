60fa4f4ddab0af64d4aab427355d73d5e4740a26
Paweł Jastrzębski <pawelj@iosphe.re>
2016-12-28 09:24:32 +0100

    Tweaked table appearance

--- REFlexStructures.lua
+++ REFlexStructures.lua
@@ -125,6 +125,12 @@ RE.BGStructure = {
     ["name"] = L["Date"],
     ["width"] = 110,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Time", 0) end,
+    ["bgcolor"] = {
+      ["r"] = 0.15,
+      ["g"] = 0.15,
+      ["b"] = 0.15,
+      ["a"] = 1.0
+    },
     ["align"] = "CENTER"
   },
   {
@@ -132,63 +138,57 @@ RE.BGStructure = {
     ["width"] = 130,
     ["color"] = RE.GetMapColor,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Map", 0) end,
-    ["bgcolor"] = {
-      ["r"] = 0.15,
-      ["g"] = 0.15,
-      ["b"] = 0.15,
-      ["a"] = 1.0
-    },
     ["align"] = "CENTER"
   },
   {
     ["name"] = AUCTION_DURATION,
     ["width"] = 70,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Duration", 0) end,
+    ["bgcolor"] = {
+      ["r"] = 0.15,
+      ["g"] = 0.15,
+      ["b"] = 0.15,
+      ["a"] = 1.0
+    },
     ["align"] = "CENTER"
   },
   {
     ["name"] = WIN,
     ["width"] = 50,
-    ["bgcolor"] = {
-      ["r"] = 0.15,
-      ["g"] = 0.15,
-      ["b"] = 0.15,
-      ["a"] = 1.0
-    },
     ["align"] = "CENTER"
   },
   {
     ["name"] = "KB",
     ["width"] = 50,
-    ["align"] = "CENTER"
-  },
-  {
-    ["name"] = "HK",
-    ["width"] = 50,
     ["bgcolor"] = {
       ["r"] = 0.15,
       ["g"] = 0.15,
       ["b"] = 0.15,
       ["a"] = 1.0
     },
+    ["align"] = "CENTER"
+  },
+  {
+    ["name"] = "HK",
+    ["width"] = 50,
     ["align"] = "CENTER"
   },
   {
     ["name"] = DAMAGE,
     ["width"] = 65,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Damage", 12) end,
-    ["align"] = "CENTER"
-  },
-  {
-    ["name"] = SHOW_COMBAT_HEALING,
-    ["width"] = 65,
-    ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Healing", 13) end,
     ["bgcolor"] = {
       ["r"] = 0.15,
       ["g"] = 0.15,
       ["b"] = 0.15,
       ["a"] = 1.0
     },
+    ["align"] = "CENTER"
+  },
+  {
+    ["name"] = SHOW_COMBAT_HEALING,
+    ["width"] = 65,
+    ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Healing", 13) end,
     ["align"] = "CENTER"
   },
   {
@@ -200,17 +200,17 @@ RE.BGStructure = {
       ["b"] = 0,
       ["a"] = 1.0
     },
-    ["align"] = "CENTER"
-  },
-  {
-    ["name"] = RATING,
-    ["width"] = 65,
     ["bgcolor"] = {
       ["r"] = 0.15,
       ["g"] = 0.15,
       ["b"] = 0.15,
       ["a"] = 1.0
     },
+    ["align"] = "CENTER"
+  },
+  {
+    ["name"] = RATING,
+    ["width"] = 65,
     ["align"] = "CENTER"
   }
 }
@@ -220,6 +220,12 @@ RE.ArenaStructure = {
     ["name"] = L["Date"],
     ["width"] = 110,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Time", 0) end,
+    ["bgcolor"] = {
+      ["r"] = 0.15,
+      ["g"] = 0.15,
+      ["b"] = 0.15,
+      ["a"] = 1.0
+    },
     ["align"] = "CENTER"
   },
   {
@@ -227,79 +233,73 @@ RE.ArenaStructure = {
     ["width"] = 60,
     ["color"] = RE.GetMapColorArena,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Map", 0) end,
-    ["bgcolor"] = {
-      ["r"] = 0.15,
-      ["g"] = 0.15,
-      ["b"] = 0.15,
-      ["a"] = 1.0
-    },
     ["align"] = "CENTER"
   },
   {
     ["name"] = TEAM,
     ["width"] = 100,
-    ["align"] = "CENTER"
-  },
-  {
-    ["name"] = "MMR",
-    ["width"] = 50,
     ["bgcolor"] = {
       ["r"] = 0.15,
       ["g"] = 0.15,
       ["b"] = 0.15,
       ["a"] = 1.0
     },
+    ["align"] = "CENTER"
+  },
+  {
+    ["name"] = "MMR",
+    ["width"] = 50,
     ["align"] = "CENTER"
   },
   {
     ["name"] = ENEMY,
     ["width"] = 100,
-    ["align"] = "CENTER"
-  },
-  {
-    ["name"] = "MMR",
-    ["width"] = 50,
     ["bgcolor"] = {
       ["r"] = 0.15,
       ["g"] = 0.15,
       ["b"] = 0.15,
       ["a"] = 1.0
     },
+    ["align"] = "CENTER"
+  },
+  {
+    ["name"] = "MMR",
+    ["width"] = 50,
     ["align"] = "CENTER"
   },
   {
     ["name"] = AUCTION_DURATION,
     ["width"] = 60,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Duration", 0) end,
+    ["bgcolor"] = {
+      ["r"] = 0.15,
+      ["g"] = 0.15,
+      ["b"] = 0.15,
+      ["a"] = 1.0
+    },
     ["align"] = "CENTER"
   },
   {
     ["name"] = DAMAGE,
     ["width"] = 70,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Damage", 12) end,
-    ["bgcolor"] = {
-      ["r"] = 0.15,
-      ["g"] = 0.15,
-      ["b"] = 0.15,
-      ["a"] = 1.0
-    },
     ["align"] = "CENTER"
   },
   {
     ["name"] = SHOW_COMBAT_HEALING,
     ["width"] = 70,
     ["comparesort"] = function (self, rowa, rowb, sortbycol) return RE:CustomSort(self, rowa, rowb, sortbycol, "Healing", 13) end,
-    ["align"] = "CENTER"
-  },
-  {
-    ["name"] = RATING,
-    ["width"] = 50,
     ["bgcolor"] = {
       ["r"] = 0.15,
       ["g"] = 0.15,
       ["b"] = 0.15,
       ["a"] = 1.0
     },
+    ["align"] = "CENTER"
+  },
+  {
+    ["name"] = RATING,
+    ["width"] = 50,
     ["align"] = "CENTER"
   }
 }
