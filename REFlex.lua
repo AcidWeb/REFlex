59d4832f2203db823a1f578ba609dcfeb7adaa6d
Paweł Jastrzębski <pawelj@iosphe.re>
2017-02-12 08:28:50 +0100

    Version bump

--- REFlex.lua
+++ REFlex.lua
@@ -8,8 +8,8 @@ local LDBI = LibStub("LibDBIcon-1.0")
 local TOAST = LibStub("LibToast-1.0")
 local QTIP = LibStub("LibQTip-1.0")
 
-RE.Version = 220
-RE.VersionStr = "2.2.0"
+RE.Version = 221
+RE.VersionStr = "2.2.1"
 RE.FoundNewVersion = false
 
 RE.DataSaved = false
