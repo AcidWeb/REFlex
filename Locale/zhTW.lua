1af64012e5e9a72f44540e1089a00341cdce1be2
Paweł Jastrzębski <pawelj@iosphe.re>
2017-05-31 10:56:36 +0200

    Added zhTW translation

---
+++ Locale/zhTW.lua
@@ -0,0 +1,19 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("REFlex", "zhTW")
+if not L then return end
+
+L["Date"] = "日期"
+L["Display server names"] = "顯示伺服器名稱"
+L["Display toast with battleground summary after completed match."] = "完全配對後顯示戰場總結的彈出式訊息。"
+L["Enable battleground summary"] = "啟用戰場總結"
+L["Hide minimap button"] = "隱藏小地圖按鈕"
+L["Hold SHIFT and ALT to display extended tooltips."] = "按住 Shift 和 Alt 鍵顯示更多滑鼠提示內容。"
+L["Hold SHIFT to display tooltips."] = "按住 Shift 和 Alt 鍵顯示滑鼠提示。"
+L["New version released!"] = "已有新版本!"
+L["Place"] = "地點"
+L["Purge database"] = "清除資料庫"
+L["Purge previous seasons"] = "清除上一回合資料"
+L["Select start and end date by clicking it."] = "點一下選擇開啟和結束日期"
+L["Show main window"] = "顯示主視窗"
+L["Show player server name in match detail tooltip."] = "在配對詳細內容的滑鼠提示說明中顯示玩家伺服器名稱。"
+L["This month"] = "本月份"
+L["WARNING! This operation is not reversible!"] = "警告! 這個動作無法還原!"
