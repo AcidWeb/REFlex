2626ba5430bacaeb50d42c6a662ee04b3d5a7843
Paweł Jastrzębski <pawelj@iosphe.re>
2019-03-18 17:16:57 +0100

    Fixed possible UI errors on non-english clients

--- REFlex.lua
+++ REFlex.lua
@@ -105,7 +105,7 @@ local RegisterAddonMessagePrefix = _G.C_ChatInfo.RegisterAddonMessagePrefix
 local SendAddonMessage = _G.C_ChatInfo.SendAddonMessage
 local ElvUI = _G.ElvUI
 
-RE.Version = 266
+RE.Version = 267
 RE.LastSquash = 1531828800
 RE.FoundNewVersion = false
 
