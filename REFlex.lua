efa845f57ca350d91556d125f9ad5ddafaf0167e
Paweł Jastrzębski <pawelj@iosphe.re>
2023-01-24 23:43:10 +0100

    TOC bump

--- REFlex.lua
+++ REFlex.lua
@@ -63,7 +63,7 @@ local SendAddonMessage = _G.C_ChatInfo.SendAddonMessage
 local PlaySound = _G.PlaySound
 local ElvUI = _G.ElvUI
 
-RE.Version = 3304
+RE.Version = 3305
 RE.LastSquash = 1602662400
 RE.FoundNewVersion = false
 
